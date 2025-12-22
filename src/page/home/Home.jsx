e9945c14915c320aeb7311c6b9133a9ea4d7c6b4
Dhaya3132 <dhayalanu103@gmail.com>
2025-12-22 10:47:27 +0530

    PO1-T002 : #About page content

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="header_section flex items-center gap-5 border-b border-gray-200 pb-5">
+
+      <div className="header_section flex items-center gap-5 pb-5">
 
         <div className="header_title">
           <h2 className="font-bold text-5xl mb-2">Hi, I'm Dhayalan 👋</h2>
@@ -20,6 +21,12 @@ const Home = () => {
 
 
       </div>
+
+      <div className="about_section py-5">
+        <h4 className="mb-2 font-semibold text-2xl">About</h4>
+        <p className="mb-2 font-normal text-lg text-gray-500">I’m a Frontend Developer with 2+ years of experience building scalable, high-performance web applications for retail and POS systems. I specialize in React.js, modern JavaScript, and state management using Redux and Redux-Saga, delivering production-ready features used across 140+ retail stores.</p>
+      </div>
+
     </div>
   );
 };
